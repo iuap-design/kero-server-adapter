740469186dff2f058d22094676748962da06025e
guoyff <guoyff@yonyou.com>
2016-10-18 16:53:05 +0800

    update(config):webpack&src

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,25 @@ var path = require('path')
 var webpack = require('webpack')
 
 var config = {
-
+  entry: {
+    index: path.resolve(__dirname, './src/index.js')
+  },
+  output: {
+    path: path.resolve(__dirname, './lib'),
+    filename: 'kero-server.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  plugins: [
+    new webpack.BannerPlugin('版本：1.0.0\n日期：2016-10-18\n作者：Yonyou-FED')
+  ]
 }
 
 module.exports = config
